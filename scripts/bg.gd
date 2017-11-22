daca4d0a7e2f825538d9a066e36f4c488c34621f
Akerke <akerke.kass@gmail.com>
2017-11-22 17:37:37 +0600

    Initial commit

---
+++ scripts/bg.gd
@@ -0,0 +1,27 @@
+extends TextureFrame
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	get_node("Author").set_text("История про маленького принца")
+	get_node("SamplePlayer").play("pp")
+	get_node("MyKnight").get_node("AnimationPlayer").play("run")
+	
+func _on_AnimationPlayer_finished():
+	get_node("MyKnight").get_node("Label").get_node("textAnim").play("char")
+	#get_node("MyKnight").get_node("Label").set_text("А ты хочешь чтобы я зашел в дверь?")
+	
+func _on_YesButton_pressed():
+	get_tree().change_scene("res://scenes/two.tscn")
+	#need to set the scene to second one
+
+func _on_NoButton_pressed():
+	pass #set scene to three
+
+
+func _on_textAnim_finished():
+	get_node("HBoxContainer").show() # replace with function body
---
+++ scripts/two.gd
@@ -0,0 +1,10 @@
+extends TextureFrame
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	get_node("AuthorAnim").play("Scene2Author")
