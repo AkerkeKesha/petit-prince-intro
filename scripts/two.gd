d1d84cc8d4f7b50e97653b29581a45b27b6d9da4
AkerkeKesha <akerke.kass@gmail.com>
2017-11-27 12:29:55 +0600

    so practiced godot broadcast message
    added function call to second scene animation

--- scripts/two.gd
+++ scripts/two.gd
@@ -7,4 +7,15 @@ extends TextureFrame
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	get_node("pilot").hide()
 	get_node("AuthorAnim").play("Scene2Author")
+	
+	
+	
+func pilot(hideOrNot):
+	print("we are calling function!", hideOrNot)
+	if hideOrNot == 1:
+		get_node("pilot").show()
+	elif hideOrNot == 0:
+		get_node("pilot").hide()
+	
